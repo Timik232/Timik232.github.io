888c80ba75f54da374c23d3b195ec65373aa30b9
Timur <100406268+Timik232@users.noreply.github.com>
2022-12-02 01:31:09 +0300

    added turnoff button

--- js/submit.js
+++ js/submit.js
@@ -1,7 +1,22 @@
 let submit =  document.querySelector('.sendMessage');
+let turnoff = document.getElementById("turnoff");
+let box = document.querySelector(".box");
+let isTurn = false;
 
 submit.addEventListener('submit', function(e) {
   e.preventDefault();
   alert("Сообщение отправлено");
   location.reload ()
+});
+
+turnoff.addEventListener("click", () => {
+  if (!isTurn){
+    box.style.visibility = "hidden";
+    isTurn = true;
+  }
+  else {
+    box.style.visibility = "visible";
+
+    isTurn = false;
+  }
 });
